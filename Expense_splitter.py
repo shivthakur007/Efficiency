173bc00421302467a785c716884092d6024af643
Shiv Thakur <125086310+shivthakur007@users.noreply.github.com>
2026-01-18 20:04:38 +0530

    Update Expense_splitter.py

--- Expense_splitter.py
+++ Expense_splitter.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-st.set_page_config(page_title="Trip Expense Splitter", layout="centered")
+st.set_page_config(page_title="Trip Expense Splitter", layout="Right")
 
 st.title("💸 Trip Expense Splitter")
 
